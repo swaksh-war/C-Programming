1f14c3f15b8811c272e1780855b11b84ea68c752
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-12-11 03:06:03 +0530

    main

--- ca2ques2.c
+++ ca2ques2.c
@@ -14,16 +14,9 @@ int max_of_four(int a, int b, int c, int d)
 }
 int main()
 {
-
     int m, n, o, p, res;
-    printf("Enter The 1st num: ");
-    scanf("%d", &m);
-    printf("Enter The 2nd num: ");
-    scanf("%d", &n);
-    printf("Enter The 3rd num: ");
-    scanf("%d", &o);
-    printf("Enter The 4th num: ");
-    scanf("%d", &p);
+    printf("Enter the nums: ");
+    scanf("%d%d%d%d",&m,&n,&o,&p);
     res = max_of_four(m, n, o, p);
     printf("The greatest value: %d", res);
 }
