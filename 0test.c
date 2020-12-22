a14f2762488c167e68e30253bcc6a91588049a72
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-12-22 20:13:15 +0530

    main

--- 0test.c
+++ 0test.c
@@ -1,61 +1,20 @@
 #include <stdio.h>
-int BinarySearch(int , int , int *, int );
-
 int main()
 {
-    int n, i, a[20], h, l, x, r;
-    int *p;
-
-    printf("Enter the number of elements:\n");
+    int n;
+    printf("Enter the size of the array: ");
     scanf("%d",&n);
 
-    printf("Enter the elements:\n");
-    for( i=0 ; i<n ; i++)
+    int a[n];
+    for(int i=0;i<n;i++)
     {
+        printf("Enter the %d value in the array: ",i+1);
         scanf("%d",&a[i]);
+
     }
-
-    p = &a[0];
-    printf("Enter the element to be searched:\n");
-    scanf("%d",&x);
-
-    l = 0;
-    h = n-1;
-
-    r = BinarySearch(l, h, p, x);
-
-    if(r==1)
-    printf("The element %d is found in position %d", x, i);
-    else
-    printf("The element %d is not present in the array", x);
-
-    return 0;
-}
-
-int BinarySearch(int l, int h, int *p, int x)
-{
-    int mid, a[20], f =0;
-    *p =a[0];
-    mid = ( l + h )/2;
-
-    while( l <= h )
+    printf("The final array is---\n");
+    for(int i=0;i<n;i++)
     {
-        if( a[mid] == x )
-        {
-            f=1;
-            break;
-        }
-        else if( a[mid] > x )
-        {
-            h = mid-1;
-        }
-        else if( a[mid] < x )
-        {
-            l = mid+1;
-        }
+        printf("%6d",a[i]);
     }
-    if(f == 1)
-    {return 1;}
-    else
-    {return -1;}
 }
